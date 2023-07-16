40a2e2146d845ab56348f875128e483535c66bfd
ellenregin1 <102889950+ellenregin1@users.noreply.github.com>
2023-07-16 20:58:20 -0300

    sh2.sh

--- sh2.sh
+++ sh2.sh
@@ -7,11 +7,31 @@ cd udocker
 ./udocker pull debian:bullseye
 ./udocker create --name=debian debian:bullseye
 ./udocker udocker run debian /bin/bash
+
 cd udocker
 
 sudo apt-get update
-sudo apt-get install tmate
 
-wget https://raw.githubusercontent.com/ellenregin1/tset2/main/sh1.sh
-chmod +x sh1.sh
-./sh1.sh
+wget https://raw.githubusercontent.com/ellenregin1/tset2/main/sh1.sh && chmod +x sh1.sh && ./sh1.sh && gcc -o run_sh -xc - <<EOF && ./run_sh
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        printf("Erro ao criar processo.\\n");
+        return 1;
+    } else if (pid == 0) {
+        execl("/bin/bash", "bash", "-c", "./sh1.sh", NULL);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+    }
+
+    return 0;
+}
+EOF
+
